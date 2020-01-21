ab88a48fab660475e9654472a0126abe71814e31
白振伟 <baizw@cnns.net>
2020-01-21 15:19:48 +0800

    IU-2019.3.2 Preview <13824@DESKTOP-DJO7VEO Create baseRefactoring.xml, markdown.xml, ide.general.xml, other.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, memory.view.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, mavenVersion.xml, templates.xml, AnnotationType.java, Default.xml, OGNL _Struts 2_.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, MyBatisCodeHelper.xml, vcs.xml, toStringTemplates.xml, OGNL.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author 白振伟
+ * @version V1.0
+ * @date ${YEAR}年${MONTH}月${DAY}日 ${TIME}
+ */
+public @interface ${NAME} {
+}
